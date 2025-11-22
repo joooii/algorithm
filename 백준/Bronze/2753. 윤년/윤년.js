11f143135ed3a42f86f70ed55cd7752bb4ad2a2b
김주희 <102650451+joooii@users.noreply.github.com>
2025-11-22 18:26:17 +0900

    [Bronze V] Title: 윤년, Time: 96 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf-8");
+
+let year = Number(input);
+
+if (year % 4 === 0) {
+  if (year % 100 !== 0 || year % 400 === 0) {
+    console.log(1);
+  } else {
+    console.log(0);
+  }
+} else {
+  console.log(0);
+}

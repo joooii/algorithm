5bf661f90656887b8f96c58f6aa120802c5afd79
김주희 <102650451+joooii@users.noreply.github.com>
2026-05-23 12:54:30 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 32 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -0,0 +1,14 @@
+function solution(angle) {
+    switch(true) {
+        case angle < 90 && angle > 0:
+            return 1;
+        case angle === 90:
+            return 2;
+        case angle > 90 && angle < 180:
+            return 3;
+        case angle === 180:
+            return 4;
+        default:
+            return null;
+    }
+}
